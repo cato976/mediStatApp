ae32b5fe0eed3fc872e78c1479bdbd99e62816dd
Andre Cato <andrecato@gmail.com>
2020-06-07 13:12:27 -0400

    Add Auth files for Okta and add jest to run test

---
+++ auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  oidc: {
+    clientId: '0oaeb2y5ju25ezLiF4x6',
+    redirectUri: 'com.okta.dev-823843:/callback',
+    endSessionRedirectUri: 'com.okta.dev-823843:/callback',
+    discoveryUri: 'https://dev-823843.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  },
+};
